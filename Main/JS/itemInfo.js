03b5939f9c79534a10e3ab91e9388ee86a2c76b1
WiciokS <77240194+WiciokS@users.noreply.github.com>
2024-05-30 17:33:02 +0300

    Added item info display and added update review and item info on the page

---
+++ Main/JS/itemInfo.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const itemInfoButton = document.getElementById('itemInfoButton');
+    const performItemInfoSearchButton = document.getElementById('performItemInfoSearchButton');
+    const itemInfoSection = document.getElementById('itemInfoSection');
+
+    itemInfoButton.addEventListener('click', () => {
+        showSection('itemInfoSection');
+    });
+
+    performItemInfoSearchButton.addEventListener('click', itemInfoSearch);
+
+    async function itemInfoSearch() {
+        clearItemInfoResults();
+        const itemName = document.getElementById('ItemNameInfoSearch').value;
+
+        const queryParams = new URLSearchParams({
+            itemName: itemName
+        }).toString();
+
+        try {
+            const response = await fetch(`http://84.55.60.45:443/itemInfo?${queryParams}`);
+            const data = await response.json();
+
+            if (data.success) {
+                const itemInfo = data.message;
+                console.log('Item Info Data:', itemInfo);
+
+                displayItemInfo(itemInfo);
+            } else {
+                alert('Error fetching item info.');
+            }
+        } catch (error) {
+            console.error("Error fetching item info:", error);
+        }
+    }
+
+    function clearItemInfoResults() {
+        const itemInfoResults = document.getElementById('itemInfoResults');
+        itemInfoResults.innerHTML = '';
+    }
+
+    function displayItemInfo(itemInfo) {
+        const itemInfoResults = document.getElementById('itemInfoResults');
+        const imagesHtml = itemInfo.Images.map(image => `<img src="${image}" alt="${itemInfo['Item Name']}" style="max-width: 100%; height: auto; display: block; margin-bottom: 10px;" />`).join('');
+        itemInfoResults.innerHTML = `
+            <h2>Item Name: ${itemInfo['Item Name']}</h2>
+            <p>Alternate Name: ${itemInfo['Alternate Name']}</p>
+            <p>Description: ${itemInfo['Description']}</p>
+            <div>${imagesHtml}</div>
+        `;
+    }
+});
--- Main/JS/search.js
+++ Main/JS/search.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('searchButton');
     const performSearchButton = document.getElementById('performSearchButton');
+    const performDomainSearchButton = document.getElementById('performDomainSearchButton');
+    const performUserSearchButton = document.getElementById('performUserSearchButton');
+    const performItemInDomainSearchButton = document.getElementById('performItemInDomainSearchButton');
     const searchSection = document.getElementById('searchSection');
 
     searchButton.addEventListener('click', () => {
@@ -8,14 +11,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     performSearchButton.addEventListener('click', itemSearch);
+    performDomainSearchButton.addEventListener('click', domainSearch);
+    performUserSearchButton.addEventListener('click', userSearch);
+    performItemInDomainSearchButton.addEventListener('click', itemInDomainSearch);
 
     async function itemSearch() {
+        clearResults();
         const itemName = document.getElementById('ItemNameSearch').value;
-
+        console.log('Item Name:', itemName);
         const queryParams = new URLSearchParams({
             itemName: itemName
         }).toString();
-
+        console.log('Query Params:', queryParams);
         try {
             const itemResponse = await fetch(`http://84.55.60.45:443/getItem?${queryParams}`);
             const itemData = await itemResponse.json();
@@ -45,6 +52,101 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    async function domainSearch() {
+        clearResults();
+        const domainName = document.getElementById('DomainNameSearch').value;
+        console.log('Domain Name:', domainName);
+        const queryParams = new URLSearchParams({
+            domainName: domainName
+        }).toString();
+        console.log('Query Params:', queryParams);
+        try {
+            const domainResponse = await fetch(`http://84.55.60.45:443/getDomain?${queryParams}`);
+            const domainData = await domainResponse.json();
+
+            if (domainData.success) {
+                const domain = domainData.message;
+                console.log('Domain Data:', domain);
+
+                displayDomainData(domain);
+
+                const reviewsResponse = await fetch(`http://84.55.60.45:443/getReviewsForDomain?${queryParams}`);
+                const reviewsData = await reviewsResponse.json();
+
+                if (reviewsData.success) {
+                    const reviews = reviewsData.message;
+                    console.log('Reviews Data:', reviews);
+
+                    displayReviews(reviews);
+                } else {
+                    alert('Error fetching reviews for the domain.');
+                }
+            } else {
+                alert('Error fetching domain data.');
+            }
+        } catch (error) {
+            console.error("Error fetching domain data or reviews:", error);
+        }
+    }
+
+    async function userSearch() {
+        clearResults();
+        const userAddress = document.getElementById('UserAddressSearch').value;
+        console.log('User Address:', userAddress);
+        const queryParams = new URLSearchParams({
+            address: userAddress
+        }).toString();
+        console.log('Query Params:', queryParams);
+        try {
+            const userResponse = await fetch(`http://84.55.60.45:443/getReviewsOfUser?${queryParams}`);
+            const userData = await userResponse.json();
+
+            if (userData.success) {
+                const reviews = userData.message;
+                console.log('User Reviews Data:', reviews);
+
+                displayReviews(reviews);
+            } else {
+                alert('Error fetching reviews for the user.');
+            }
+        } catch (error) {
+            console.error("Error fetching user reviews:", error);
+        }
+    }
+
+    async function itemInDomainSearch() {
+        clearResults();
+        const itemName = document.getElementById('ItemNameInDomainSearch').value;
+        const domainName = document.getElementById('DomainNameForItemSearch').value;
+        console.log('Item Name:', itemName);
+        console.log('Domain Name:', domainName);
+        const queryParams = new URLSearchParams({
+            itemName: itemName,
+            domainName: domainName
+        }).toString();
+        console.log('Query Params:', queryParams);
+        try {
+            const reviewsResponse = await fetch(`http://84.55.60.45:443/getReviewsForItemOfDomain?${queryParams}`);
+            const reviewsData = await reviewsResponse.json();
+
+            if (reviewsData.success) {
+                const reviews = reviewsData.message;
+                console.log('Reviews for Item in Domain Data:', reviews);
+
+                displayReviews(reviews);
+            } else {
+                alert('Error fetching reviews for the item in the domain.');
+            }
+        } catch (error) {
+            console.error("Error fetching reviews for the item in the domain:", error);
+        }
+    }
+
+    function clearResults() {
+        const searchResults = document.getElementById('searchResults');
+        searchResults.innerHTML = '';
+    }
+
     function displayItemData(item) {
         const searchResults = document.getElementById('searchResults');
         searchResults.innerHTML = `
@@ -54,11 +156,20 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
     }
 
+    function displayDomainData(domain) {
+        const decodedDomainName = decodeURIComponent(domain.name);
+        const searchResults = document.getElementById('searchResults');
+        searchResults.innerHTML = `
+            <h2>Domain: ${decodedDomainName}</h2>
+        `;
+    }
+
     function displayReviews(reviews) {
         const searchResults = document.getElementById('searchResults');
         const reviewsHtml = reviews.map(review => `
             <div class="review">
                 <p><strong>Reviewer:</strong> ${review.reviewer}</p>
+                <p><strong>Item:</strong> ${review.itemName}</p>
                 <p><strong>Rating:</strong> ${review.rating}</p>
                 <p><strong>Comment:</strong> ${review.comment}</p>
             </div>
